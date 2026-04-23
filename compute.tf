afdffe3713d140bc3cddc52d4c36e841d390ec95
Deus lo Vult <moises.barrera.dev@gmail.com>
2026-04-23 00:18:51 -0400

    Assign public IP to web instance

--- compute.tf
+++ compute.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0ec10929233384c7f"
-  instance_type = "t3.micro"
-  subnet_id     = aws_subnet.public.id
+  ami                         = "ami-0ec10929233384c7f"
+  associate_public_ip_address = true
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public.id
   # security_group_ids = [aws_security_group.public.id]
 }
