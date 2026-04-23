06f5b9dd824e99b1e7bc22d938420e37bf108f05
Deus lo Vult <moises.barrera.dev@gmail.com>
2026-04-23 00:00:05 -0400

    Add VPC, subnet, IGW, route table and instance

---
+++ compute.tf
@@ -0,0 +1,6 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0ec10929233384c7f"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public.id
+  # security_group_ids = [aws_security_group.public.id]
+}
--- networking.tf
+++ networking.tf
@@ -1,20 +1,40 @@
+locals {
+  common_tags = {
+    ManagedBy = "Terraform"
+    Project   = "resources"
+  }
+}
+
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name      = "Nginx-resources"
-    ManagedBy = "Terraform"
-    Project   = "resources-project"
-  }
+  tags       = merge(local.common_tags, { Name = "VPC-main" })
 }
 
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
+  tags       = merge(local.common_tags, { Name = "Subnet-public-main" })
+}
 
-  tags = {
-    Name      = "Public Subnet"
-    ManagedBy = "Terraform"
-    Project   = "resources-project"
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags   = merge(local.common_tags, { Name = "InternetGateway-main" })
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
   }
+  tags = merge(local.common_tags, { Name = "RouteTable-public-main" })
+}
+
+# Permite asociar la tabla de rutas pública con la
+# subred pública para permitir el acceso a Internet
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
