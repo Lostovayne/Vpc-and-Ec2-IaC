9fb001d9527691ce7d3900285f6c4c144ddc9765
Deus lo Vult <moises.barrera.dev@gmail.com>
2026-04-22 22:37:30 -0400

    Add public subnet and rename VPC resource

--- networking.tf
+++ networking.tf
@@ -1,9 +1,20 @@
-resource "aws_vpc" "this" {
-	cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
-	tags = {
-		Name = "Nginx-resources"
-		ManagedBy = "Terraform"
-		Project = "resources-project"
-	}
+  tags = {
+    Name      = "Nginx-resources"
+    ManagedBy = "Terraform"
+    Project   = "resources-project"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name      = "Public Subnet"
+    ManagedBy = "Terraform"
+    Project   = "resources-project"
+  }
 }
